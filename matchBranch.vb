bc73c778623326139f50c476252a51ae58a4103c
CAM\asoneriu <asoneriucam@gmail.com>
2018-03-01 11:45:46 -0800

    d

--- matchBranch.vb
+++ matchBranch.vb
@@ -1,5 +1,16 @@
 Private Function matchBranch()
 
 
+If (Left(ActiveCell.Offset(0, -1).Value, 2) = "CV") Then
+ActiveCell.Value = "Central Valley"
+    ElseIf (Left(ActiveCell.Offset(0, -1).Value, 2) = "IE") Then
+        ElseIf (Left(ActiveCell.Offset(0, -1).Value, 2) = "LA") Then
+        ActiveCell.Value = "Los Angeles"
+            ElseIf (Left(ActiveCell.Offset(0, -1).Value, 2) = "OC") Then
+            ActiveCell.Value = "Orange County"
+                ElseIf (Left(ActiveCell.Offset(0, -1).Value, 2) = "SD") Then
+                ActiveCell.Value = "San Diego"
+                
+End If
 
 End Function
