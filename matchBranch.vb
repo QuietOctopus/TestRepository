7eda53ddf01007470f74102879ec2b8536dbbd21
QuietOctopus <36968500+QuietOctopus@users.noreply.github.com>
2018-03-01 11:51:31 -0800

    Update matchBranch.vb

--- matchBranch.vb
+++ matchBranch.vb
@@ -1,3 +1,29 @@
+Private Sub createBranchColumn()
+
+Application.ScreenUpdating = False
+    Range("C4").Select 'Property Column
+    ActiveCell.Offset(0, 1).Columns("A:A").EntireColumn.Select
+    Selection.Insert Shift:=xlToRight, CopyOrigin:=xlFormatFromLeftOrAbove
+    ActiveCell.Offset(3, 0).Range("A1").Select
+    ActiveCell.FormulaR1C1 = "Branch"
+    ActiveCell.Offset(1, 0).Range("A1").Select
+Do Until (ActiveCell.Offset(0, -1) = vbNullString)
+If (Left(ActiveCell.Offset(0, -1).Value, 2) = "CV") Then
+ActiveCell.Value = "Central Valley"
+    ElseIf (Left(ActiveCell.Offset(0, -1).Value, 2) = "IE") Then
+    ActiveCell.Value = "Inland Empire"
+        ElseIf (Left(ActiveCell.Offset(0, -1).Value, 2) = "LA") Then
+        ActiveCell.Value = "Los Angeles"
+            ElseIf (Left(ActiveCell.Offset(0, -1).Value, 2) = "OC") Then
+            ActiveCell.Value = "Orange County"
+                ElseIf (Left(ActiveCell.Offset(0, -1).Value, 2) = "SD") Then
+                ActiveCell.Value = "San Diego"
+End If
+ActiveCell.Offset(1, 0).Activate
+Loop
+Application.ScreenUpdating = True
+End Sub
+                
 Private Function matchBranch()
 
 
